300a6b0effef260f1c6ef0b01ee59100ff88eb8a
oncomouse <oncomouse@gmail.com>
2015-02-09 15:05:56 -0700

    Can require bootstrap js, if needed (correct path included).

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,7 +2,7 @@
 //= require 'underscore.string/dist/underscore.string'
 //= require 'underscore.inflection/lib/underscore.inflection'
 //= require 'jquery/dist/jquery'
-// require 'bootstrap/dist/js/bootstrap'
+// require 'bootstrap-sass-official/assets/javascripts/bootstrap'
 //= require 'isotope/dist/isotope.pkgd'
 //= require 'plugins'
 //= require 'site'
